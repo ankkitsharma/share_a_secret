5192b55b1f61a94535bd151faad365a91b75d2cf
ankkitsharma <ankitsharma2801@gmail.com>
2025-05-13 23:02:26 +0530

    feat: add secrets table and implement API endpoints for secrets management

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,8 +1,13 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const secretsTable = pgTable("secrets", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+  secret: varchar({ length: 255 }).notNull(),
+  decryptionKey: varchar({ length: 255 }),
+  oneTime: integer().default(0),
+  destroyAfter: timestamp(),
+  created_at: timestamp({ precision: 3 }).defaultNow(),
+  updated_at: timestamp({ precision: 3 }).defaultNow(),
+})
+
